b3cea29e6affd414a050e33a8a6f8b5bf661aa81
Ryan Cromwell <ryan@echelontouch.com>
2012-01-07 00:04:36 -0500

    added bootstrap as a submodule

--- doit.js
+++ doit.js
@@ -7,18 +7,12 @@ var fs = require('fs')
 
 
 function doit() {
-	fs.readFile('../bootstrap/lib/variables.less', function(err, data) {
+	fs.readFile('./bootstrap/lib/variables.less', function(err, data) {
 		var template = data;
 
-		fs.readFile('../bootstrap/lib/bootstrap.less', function(err, data) {
+		fs.readFile('./bootstrap/lib/bootstrap.less', function(err, data) {
 			var tobeparsed = template + '\n' + data;
 
-//
-//			var parser = new(less.Parser)({
-//				paths: ['.', './lib'], // Specify search paths for @import directives
-//				filename: 'style.less' // Specify a filename, for better error messages
-//			});
-//
 			var parser = new(less.Parser)( { 
 				paths: ['../bootstrap/lib/']
 			});
