d1ebb457bf5471c22b08dfe58cd9896b4ebc9ddf
Ryan Cromwell <ryan@echelontouch.com>
2012-01-08 20:52:05 -0500

    refactored need for creating variableset

--- spec/variables_spec.js
+++ spec/variables_spec.js
@@ -2,18 +2,16 @@
 var fs = require('fs')
 	, assert = require('assert');
 
-var variableset = function() { 
+var variables = function(){ 
 	return { 
 		toLess: function(params) { 
 			return "@linkColor: " + (typeof params.linkColor === "undefined" ? "#0069d6;" : params.linkColor);
 		}
 	};
-};
+}();
 
 describe( 'variables', function() {
 	it('custom linkColor', function() {
-		var variables = new variableset(  );
-
 		var params = { linkColor: "#0f0f0f;" };
 
 		var matches = variables.toLess(params).match(/^\@linkColor\:\s*\#0f0f0f;$/);
@@ -21,11 +19,10 @@ describe( 'variables', function() {
 	});
 
 	it('default linkColor', function() {
-		var variables = new variableset(  );
-
 		var params = { };
 
 		var matches = variables.toLess(params).match(/^\@linkColor\:\s*\#0069d6;$/);
 		assert.notEqual( matches, null);
 	});
+
 });
